d3bd8f230076d74d293bba7bee5f7b9116e427d0
manojkrishnan1 <127621202+manojkrishnan1@users.noreply.github.com>
2023-05-18 17:13:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
 // complete the given function
 
 function palindrome(str){
+	str = str.toUpperCase();
+	
+	let i=0,j=str.length-1, result = true;
+	
+    while(i<j){
+        while(str.charAt(i)==' '){
+            i++;
+        }
+        while(str.charAt(j)==' '){
+            j--;
+        }
+        if(i<j && str.charAt(i)!=str.charAt(j)){
+        result = false;
+        break;
+        }
+        i++;
+        j--;
+    }
 
+	return result;
 }
 module.exports = palindrome
